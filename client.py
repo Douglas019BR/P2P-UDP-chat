cf048a482836a3a4239e116c2f50c30849641988
DOUG <douglassermarini@gmail.com>
2023-11-09 19:54:11 -0300

    working

--- client.py
+++ client.py
@@ -9,22 +9,21 @@ class Client(protocol.DatagramProtocol):
 
     def startProtocol(self):
         print("Client started")
+
         self.server = ("127.0.0.1", 1000)
         self.send_data("Hello Server from client", self.server)
         reactor.callInThread(self.send_message)
 
     def choose_client(self):
-        self.usr_addr = input("Enter your address: "), input("Enter your port: ")
+        self.usr_addr = input("Write the address:"), int(input("Write the port:"))
 
-    def datagramReceived(self, datagram, address):
+    def datagramReceived(self, datagram, addr):
         datagram = datagram.decode("utf-8")
 
-        if address == self.server:
+        if addr == self.server:
             print("Server says: " + datagram)
-            self.choose_client()
-            return
 
-        print(f"User {address} says: " + datagram)
+        print(f"User {addr} says: " + datagram)
 
     def send_data(self, data: str, addr):
         self.transport.write(data.encode("utf-8"), addr)
@@ -32,17 +31,15 @@ class Client(protocol.DatagramProtocol):
     def send_message(self):
         while True:
             message = input("Enter message: ")
+
             if message == "choose_client":
                 self.choose_client()
                 continue
 
-            self.send_data(message, self.server)
-
-            if message == "exit":
-                break
+            self.send_data(message, self.usr_addr)
 
 
 if __name__ == "__main__":
-    port = int(input("white your port :"))
+    port = int(input("Write the port:"))
     reactor.listenUDP(port, Client())
     reactor.run()
--- server.py
+++ server.py
@@ -1,30 +1,30 @@
-from twisted.internet import reactor, protocol
+from twisted.internet import protocol
+from twisted.internet import reactor
 
 
 class Server(protocol.DatagramProtocol):
     def __init__(self):
-        self.users = {}
+        self.users = None
 
     def startProtocol(self):
         self.users = set()
 
     def send_message(self, data: str, addr):
-        self.transport.write(data, addr)
+        self.transport.write(data.encode("utf-8"), addr)
 
-    def datagramReceived(self, datagram, address):
-        print(f"Received {datagram} from{address}")
+    def datagramReceived(self, datagram, addr):
+        print(datagram, addr)
+
         try:
-            addresses = "\n".join(str(usr) for usr in self.users).enconde("utf-8")
+            addresses = "\n".join(str(user) for user in self.users)
+            self.send_message(addresses, addr)
+            for user in self.users:
+                self.send_message("New address connected: " + str(addr), user)
         except Exception as exc:
             print(exc)
-            self.send_message("No users online", address)
+            self.send_message("No users online", addr)
         else:
-            self.send_message(addresses, address)
-
-            for user in self.users:
-                self.send_message("New address connected: " + address, user)
-        finally:
-            self.users.add(address)
+            self.users.add(addr)
 
 
 if __name__ == "__main__":
